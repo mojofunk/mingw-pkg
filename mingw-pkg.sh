344f9678d52107c3b7c5491f85323ebf5bc86779
Tim Mayberry <mojofunk@gmail.com>
2015-04-30 22:00:24 +1000

    Remove old copy and pasted code

--- mingw-pkg.sh
+++ mingw-pkg.sh
@@ -9,7 +9,6 @@
 function set_msys2_env ()
 {
 	export PYTHON=/usr/bin/python3
-	#export BACKEND_OPTIONS="--with-backends=dummy"
 	export MINGW_ROOT=/mingw32
 	export PKG_CONFIG_LIBDIR=$MINGW_ROOT/lib/pkgconfig
 	export PKGCONFIG=/usr/bin/pkg-config
