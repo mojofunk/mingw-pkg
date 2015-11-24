5bec42e22d1667b36a48f928e5e21445b479bdd5
Tim Mayberry <mojofunk@gmail.com>
2015-11-24 20:40:57 +1000

    Install portaudio source into package and add directory to ardour debug script
    This allows gdb to find the portaudio source code when debugging

--- packages/ardour/make-debug-script.sh
+++ packages/ardour/make-debug-script.sh
@@ -3,6 +3,6 @@
 echo "Creating debug.bat"
 cat > $PKG_INSTALL_DIR/debug.bat << EOF
 cd bin
-START gdb.exe -iex "dir ../src/gtk2_ardour" -iex "set logging overwrite on" -iex "set height 0" -iex "set logging on %UserProfile%\\${PRODUCT_NAME}-debug.log" ${PRODUCT_EXE}
+START gdb.exe -iex "dir ../src/gtk2_ardour" -iex "dir ../src/portaudio/src" -iex "set logging overwrite on" -iex "set height 0" -iex "set logging on %UserProfile%\\${PRODUCT_NAME}-debug.log" ${PRODUCT_EXE}
 EOF
 
